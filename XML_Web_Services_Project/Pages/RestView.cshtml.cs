38f04e06f4001825a291bbd02af457f58aefe808
Pavan Motagi <98205727+pavanmotagi@users.noreply.github.com>
2022-11-09 00:04:43 -0500

    Added RestView.cshtml.cs

---
+++ XML_Web_Services_Project/Pages/RestView.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NeighborhoodFriend_RestaurantData;
+
+namespace XML_Web_Services_Project.Pages
+{
+    public class RestViewModel : PageModel
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public RestViewModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+            string project = "Neighborhood Friend";
+
+            var task = client.GetAsync("https://data.cincinnati-oh.gov/resource/rg6p-b3h3.json");
+            HttpResponseMessage result = task.Result;
+            List<RestaurantData> restaurants = new List<RestaurantData>();
+            if (result.IsSuccessStatusCode)
+            {
+                Task<string> readString = result.Content.ReadAsStringAsync();
+                string jsonString = readString.Result;
+                restaurants = RestaurantData.FromJson(jsonString);
+            }
+
+            ViewData["RestDatas"] = restaurants;
+        }
+    }
+}
