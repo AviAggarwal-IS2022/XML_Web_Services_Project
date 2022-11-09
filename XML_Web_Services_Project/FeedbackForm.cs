dd011e5c6513cfb31007ed637cbaf543289853b9
Pavan Motagi <98205727+pavanmotagi@users.noreply.github.com>
2022-11-09 17:29:54 -0500

    Added FeedbackForm.cs

---
+++ XML_Web_Services_Project/FeedbackForm.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace XML_Web_Services_Project
+{
+    public class FeedbackForm
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FeedbackFor { get; set; }
+        public int Rating { get; set; }
+        public string? Feedback { get; set; }
+    }
+}
