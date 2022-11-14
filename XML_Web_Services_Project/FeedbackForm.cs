13d473f4ea87d5ffe00ccab4cfca1a1331ee65c0
enupica <enopic20@gmail.com>
2022-11-14 14:04:03 -0500

    Code Review Recommendations

--- XML_Web_Services_Project/FeedbackForm.cs
+++ XML_Web_Services_Project/FeedbackForm.cs
@@ -4,10 +4,19 @@ namespace XML_Web_Services_Project
 {
     public class FeedbackForm
     {
+        [Required(ErrorMessage = "This field is required")]
+        [StringLength(50, MinimumLength = 1)]
         public string Name { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
+        [Phone]
         public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
         public string FeedbackFor { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [Range(1,5)]
         public int Rating { get; set; }
         public string? Feedback { get; set; }
     }
