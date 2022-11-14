5cfe61393b005d79302e770489ac27c9c3831590
singh2ru <singh2ru@mail.uc.edu>
2022-11-14 14:00:39 -0500

    My Code Review Recommendations

--- XML_Web_Services_Project/FeedbackForm.cs
+++ XML_Web_Services_Project/FeedbackForm.cs
@@ -4,10 +4,15 @@ namespace XML_Web_Services_Project
 {
     public class FeedbackForm
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string FeedbackFor { get; set; }
+        [Required]
+        public string FeedbackCategory { get; set; }
+        [Required]
+        [Range(1,10)]
         public int Rating { get; set; }
         public string? Feedback { get; set; }
     }
