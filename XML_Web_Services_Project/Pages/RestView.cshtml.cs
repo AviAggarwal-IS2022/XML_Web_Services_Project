4a5303ecc6ceec39c8361a8a407f5fa6fc0a3192
Thanmayee Muppala <muppalathanmayee@gmail.com>
2022-11-16 12:14:01 -0500

    Code Review Changes

--- XML_Web_Services_Project/Pages/RestView.cshtml.cs
+++ XML_Web_Services_Project/Pages/RestView.cshtml.cs
@@ -6,7 +6,7 @@ namespace XML_Web_Services_Project.Pages
 {
     public class RestViewModel : PageModel
     {
-        static readonly HttpClient client = new HttpClient();
+        /*static readonly HttpClient client = new HttpClient();*/
 
         private readonly ILogger<IndexModel> _logger;
 
@@ -17,18 +17,27 @@ namespace XML_Web_Services_Project.Pages
         public void OnGet()
         {
             string project = "Neighborhood Friend";
-
-            var task = client.GetAsync("https://data.cincinnati-oh.gov/resource/rg6p-b3h3.json");
-            HttpResponseMessage result = task.Result;
-            List<RestaurantData> restaurants = new List<RestaurantData>();
-            if (result.IsSuccessStatusCode)
+            try
             {
-                Task<string> readString = result.Content.ReadAsStringAsync();
-                string jsonString = readString.Result;
-                restaurants = RestaurantData.FromJson(jsonString);
+                var task = IndexModel.client.GetAsync("https://data.cincinnati-oh.gov/resource/rg6p-b3h3.json");
+                HttpResponseMessage result = task.Result;
+                List<RestaurantData> restaurants = new List<RestaurantData>();
+                if (result.IsSuccessStatusCode)
+                {
+                    Task<string> readString = result.Content.ReadAsStringAsync();
+                    string jsonString = readString.Result;
+                    restaurants = RestaurantData.FromJson(jsonString);
+                }
+
+                ViewData["RestDatas"] = restaurants;
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("Error reading Message = {0}", e.Message);
+                ViewData["Error"] = e.Message;
+
             }
 
-            ViewData["RestDatas"] = restaurants;
         }
     }
 }
