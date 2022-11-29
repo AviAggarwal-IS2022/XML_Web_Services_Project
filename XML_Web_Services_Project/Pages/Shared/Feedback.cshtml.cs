74c604de98ed5827730c6043d381c5dfa5704fbc
JahnaviGalla <jahnavigalla97@gmail.com>
2022-11-29 01:53:45 -0500

    Final Commits #1

---
+++ XML_Web_Services_Project/Pages/Shared/Feedback.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace XML_Web_Services_Project.Pages.Shared
+{
+    public class FeedbackModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
