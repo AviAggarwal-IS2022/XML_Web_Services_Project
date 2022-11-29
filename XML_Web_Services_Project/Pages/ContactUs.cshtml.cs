613c4b46975fdc2f0e489f6f71ec87ad06c99b7e
Samipyap <spamuru8@gmail.com>
2022-11-29 06:15:26 -0500

    Created contact us page

---
+++ XML_Web_Services_Project/Pages/ContactUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace XML_Web_Services_Project.Pages
+{
+    public class ContactUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
